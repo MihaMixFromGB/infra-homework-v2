bff6da6f1920a46a61a37f48562a72c4396c9dde
MihaMix <mihamix@yandex.ru>
2026-02-08 21:15:33 +0700

    feat: task-12

--- tasks/task-12/test.js
+++ tasks/task-12/test.js
@@ -1,7 +1,9 @@
 import assert from "node:assert";
 
-const res = await fetch('http://localhost:3000');
+const res = await fetch("http://localhost:3000");
 const result = await res.json();
 
 assert.equal(res.status, 200);
 assert.equal(result.id, 79);
+console.log("task-12 => test => res.status", res.status);
+console.log("task-12 => test => result.id", result.id);
